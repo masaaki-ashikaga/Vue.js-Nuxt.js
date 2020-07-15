cf4bd7b6207af93708357e85e819c320c0b2537b
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-07-15 22:41:06 +0900

    計算機能作成

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Calc v-bind:title="message" v-on:result-event="appAction" />
+    <hr>
+    <div><table v-html="log"></table></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calc from './components/Calc.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Calc
+  },
+  data: function(){
+    return{
+      message: 'CALC',
+      result: [],
+    };
+  },
+  computed: {
+    log: function(){
+      var table='<tr><th class="head">Expression</th><th class="head">Value</th></tr>';
+      for(var i in this.result){
+        table += '<tr><td>' + this.result[i][0] + '</td><th>' + this.result[i][1] + '</th></tr>';
+      }
+      return table;
+    },
+  },
+  created: function(){
+    var items = localStorage.getItem('log');
+    var logs = JSON.parse(items);
+    if(logs != null){this.resutl = logs;}
+  },
+  methods: {
+    appAction: function(exp, res){
+      this.result.unshift([exp, res]);
+      if(this.result.length > 10){
+        this.result.pop();
+      }
+      var log = JSON.stringify(this.result);
+      localStorage.setItem('log', log);
+    }
   }
 }
 </script>
